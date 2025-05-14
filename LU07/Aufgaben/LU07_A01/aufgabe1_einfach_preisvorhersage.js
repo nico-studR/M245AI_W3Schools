d0085c2e17c6e4844d8ed708ad8da3ebb767eb5d
laiba-bzz <laiba@bzz.ch>
2025-05-14 11:37:35 +0200

    Lektion 7 Aufgaben

---
+++ LU07/Aufgaben/LU07_A01/aufgabe1_einfach_preisvorhersage.js
@@ -0,0 +1,16 @@
+// aufgabe1_einfach_preisvorhersage.js
+
+// Gewichtung (w) und Bias (b)
+const w = 0.09;
+const b = 1.5;
+
+// ==== Deine Lösung ab hier ====
+
+function vorhersage(x) {
+    // TODO: Berechne y = w * x + b
+    // Runde das Ergebnis mit Math.round
+    // Gib den Preis aus: z. B. "Vorhergesagter Preis: 9 Millionen CHF"
+}
+
+// Beispielaufruf (kannst du am Ende aktivieren):
+// vorhersage(85);
