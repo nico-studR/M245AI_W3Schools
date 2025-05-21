e5741858e639d8d6819d2bb7758696f71003c12f
studern-bzz <studern@bzz.ch>
2025-05-21 15:36:25 +0200

    Lektion 17 fertig

---
+++ LU17/Aufgaben/LU17_A03/A03.js
@@ -0,0 +1,10 @@
+//Schreibe in JavaScript:
+// a) Einen Vektor, der die Werte [4, 7, 1] enthält.
+// b) Eine 2x3 Matrix, die folgende Werte enthält:
+
+const vektor = []//TODO
+
+const matrix = []//TODO
+
+console.log(vektor)
+console.log(matrix)
---
+++ LU17/Lösungen/LU17_L03/L03.js
@@ -0,0 +1,9 @@
+const vektor = [4, 7, 1];
+
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+];
+
+console.log(vektor)
+console.log(matrix)
